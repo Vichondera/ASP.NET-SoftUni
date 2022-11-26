8979d0d448987a0adfccc8f5a7005e631ac2e24c
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-11-26 19:56:44 +0200

    Create LoginViewModel.cs

---
+++ Project/All4Auto-main/All4Auto.Core/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,18 @@
+namespace All4Auto.Core.ViewModels.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [UIHint("hidden")]
+        public string? ReturnUrl { get; set; }
+    }
+}
