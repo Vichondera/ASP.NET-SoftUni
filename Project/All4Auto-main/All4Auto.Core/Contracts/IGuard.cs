88b228ce157dcd5752f8fd158e7089c0005b7307
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-11-27 19:16:36 +0200

    Add files via upload

---
+++ Project/All4Auto-main/All4Auto.Core/Contracts/IGuard.cs
@@ -0,0 +1,7 @@
+﻿namespace All4Auto.Core.Constants
+{
+    public interface IGuard
+    {
+        void AgainstNull<T>(T value, string? errorMessage = null);
+    }
+}
---
+++ Project/All4Auto-main/All4Auto.Core/Contracts/IMakeService.cs
@@ -0,0 +1,13 @@
+﻿using All4Auto.Core.Models.Vehicles;
+using All4Auto.Core.ViewModels;
+
+namespace All4Auto.Core.Contracts
+{
+    public interface IMakeService
+    {
+        // Task<int> Create(MakeModel model, int agentId);
+
+        Task<MakeQueryModel> All();
+
+    }
+}
