32902e4c15e977acca04250b15a47ea6b8c085b4
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-04 23:26:04 +0200

    Add files via upload

---
+++ Project/All4Auto-main/All4Auto/Areas/Garage/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace All4Auto.Areas.Garage.Controllers
+{
+    [Area("Garage")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project/All4Auto-main/All4Auto/Areas/Garage/Controllers/MyGarageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace All4Auto.Areas.Garage.Controllers
+{
+    public class MyGarageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
