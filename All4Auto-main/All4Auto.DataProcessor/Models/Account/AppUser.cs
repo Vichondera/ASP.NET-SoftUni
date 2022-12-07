9c6ab55e0f6b9de5d623f6e1b5c4f1e288188fa5
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-07 23:14:40 +0200

    Update and rename Project/All4Auto-main/All4Auto.DataProcessor/Models/Account/UserProfile.cs to All4Auto-main/All4Auto.DataProcessor/Models/Account/AppUser.cs

--- All4Auto-main/All4Auto.DataProcessor/Models/Account/AppUser.cs
+++ All4Auto-main/All4Auto.DataProcessor/Models/Account/AppUser.cs
@@ -3,12 +3,15 @@ namespace All4Auto.DataProcessor.Models.Account
     using Microsoft.AspNetCore.Identity;
     using System.ComponentModel.DataAnnotations;
 
-    public class UserProfile : IdentityUser
+    public class AppUser : IdentityUser
     {
         [StringLength(20)]
         public string? FirstName { get; set; }
 
         [StringLength(20)]
         public string? LastName { get; set; }
+
+       public string? UserId { get; set; }
+
     }
 }
