04ebe9391bfcad80ab28c73febcf43a18fdce3f9
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-07 21:29:47 +0200

    Update and rename Project/All4Auto-main/All4Auto.DataProcessor/All4AutoDbContext.cs to All4Auto-main/All4Auto.DataProcessor/All4AutoDbContext.cs

--- All4Auto-main/All4Auto.DataProcessor/All4AutoDbContext.cs
+++ All4Auto-main/All4Auto.DataProcessor/All4AutoDbContext.cs
@@ -1,10 +1,13 @@
-﻿namespace All4Auto.DataProcessor
+namespace All4Auto.DataProcessor
 {
     using All4Auto.DataProcessor.Configurations;
     using All4Auto.DataProcessor.Models.Account;
+    using All4Auto.DataProcessor.Models.CarGarage;
+    using All4Auto.DataProcessor.Models.Garage;
     using All4Auto.DataProcessor.Models.Vehicles;
-    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
     public class All4AutoDbContext : IdentityDbContext<UserProfile>
     {
@@ -17,16 +20,27 @@
         {
             builder.ApplyConfiguration(new MakeConfiguration());
             builder.ApplyConfiguration(new ModelConfiguration());
-            builder.ApplyConfiguration(new VehicleConfiguration());
+            
+
+            builder.Entity<OwnedModel>(entity =>
+            {
+                entity.HasKey(ct => new { ct.OwnerId, ct.ModelId });
+            });
 
             base.OnModelCreating(builder);
         }
 
         public DbSet<UserProfile> UsersProfiles { get; set; } = null!;
 
-        public DbSet<Make> Makes { get; set; } = null!;
-        public DbSet<Model> Models { get; set; } = null!;
-        public DbSet<Vehicle> Vehicles { get; set; } = null!;
+        public DbSet<CarBrand> Makes { get; set; } = null!;
+        public DbSet<CarModel> Models { get; set; } = null!;
+      //  public DbSet<Vehicle> Vehicles { get; set; } = null!;
+
+        public DbSet<Garage> Garages { get; set; } = null!;
+
+        public DbSet<Owner> Owners { get; set; } = null!;
+
+        public DbSet<OwnedModel> OwnedModels { get; set; } = null!;
 
     }
 }
