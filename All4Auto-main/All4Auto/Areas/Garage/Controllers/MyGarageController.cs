b1c9b7b9c116c8c686336a6c54ebdfdfb146a8ed
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-08 00:09:22 +0200

    Update and rename Project/All4Auto-main/All4Auto/Areas/Garage/Controllers/MyGarageController.cs to All4Auto-main/All4Auto/Areas/Garage/Controllers/MyGarageController.cs

--- All4Auto-main/All4Auto/Areas/Garage/Controllers/MyGarageController.cs
+++ All4Auto-main/All4Auto/Areas/Garage/Controllers/MyGarageController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
 namespace All4Auto.Areas.Garage.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    
     public class MyGarageController : Controller
     {
         public IActionResult Index()
