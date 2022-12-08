099e918feebef089a26c814d296c98fbd50d2426
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-08 12:34:58 +0200

    Add files via upload

---
+++ All4Auto-main/All4Auto/wwwroot/js/clock.js
@@ -0,0 +1,14 @@
+﻿window.addEventListener("load", clock);
+function clock() {
+/*
+    let clock = document.getElementById('clockId');
+    var time = Date.prototype.getTime();
+    clock.innerHTML += `${time}`;
+*/
+}
+
+// this must be in .cshtml file
+/* <h4 onclick="clock()">Click to see time!</h4>
+    <h1 id="clockId">The time is: </h1>
+    <script src="~/js/clock.js"></script>
+*/
