f1d91048ca77b02b04a1faa67c5ef2425e14de48
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-08 19:16:37 +0200

    Add files via upload

---
+++ All4Auto-main/All4Auto/Controllers/BaseController.cs
@@ -0,0 +1,38 @@
+﻿namespace All4Auto.Controllers
+{
+    using All4Auto.Core.Constants;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.AspNetCore.Authorization;
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+        public string UserFirstName
+        {
+            get
+            {
+                string firstName = string.Empty;
+
+                if (User?.Identity?.IsAuthenticated ?? false
+                    && User.HasClaim(c => c.Type == ClaimTypeConst.FirsName))
+                {
+                    firstName = User.Claims.FirstOrDefault(c => 
+                    c.Type == ClaimTypeConst.FirsName) ?.Value ?? firstName;
+                }
+                return firstName;
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                ViewBag.UserFirstName = UserFirstName;
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
