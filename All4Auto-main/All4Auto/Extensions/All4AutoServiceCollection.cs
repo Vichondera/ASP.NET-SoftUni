765819137237501dd2dd43140a30d21e49a074fe
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-08 12:28:42 +0200

    Add files via upload

---
+++ All4Auto-main/All4Auto/Extensions/All4AutoServiceCollection.cs
@@ -0,0 +1,22 @@
+﻿namespace All4Auto.Extensions
+{
+    using All4Auto.Core.Contracts;
+    using All4Auto.Core.Services;
+
+    using All4Auto.DataProcessor;
+    using All4Auto.DataProcessor.Common;
+
+    public static class All4AutoServiceCollection
+    {
+        public static IServiceCollection AddAppServices(
+           this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IVehicleService, VehicleService>();
+            //services.AddScoped<IWebHostBuilder, WebHostBuilder>();
+            //services.AddScoped<IGuard, Guard>();
+
+            return services;
+        }
+    }
+}
