d859a63eead547d944f5365f71eae6d84001f6a9
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-09 22:11:12 +0200

    Update VehicleController.cs

--- All4Auto-main/All4Auto/Controllers/VehicleController.cs
+++ All4Auto-main/All4Auto/Controllers/VehicleController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using All4Auto.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace All4Auto.Controllers
 {
     public class VehicleController : Controller
     {
-        public IActionResult Index()
+        private readonly IVehicleService vehicleService;
+
+        public VehicleController(IVehicleService _vehicleService)
         {
-            return View();
+            vehicleService= _vehicleService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async  Task<IActionResult> Brands()
+        {
+            var brands = await vehicleService.GetAllBrands();
+            
+            return View(brands);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> BrandById()
+        {
+            /*  var routeValues = Url.ActionContext.RouteData.Values;
+              stringparamName = "id";
+              string? id = routeValues[paramName];
+              var b = await vehicleService.GetBrandById(id);*/
+            var id = 5;
+            var brand = await vehicleService.GetBrandById(id);
+            return View(brand);
         }
     }
 }
