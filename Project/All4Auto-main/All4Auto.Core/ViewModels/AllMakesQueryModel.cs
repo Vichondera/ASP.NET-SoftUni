83caf84374f78403a7b125c1d28ba59c6a8c0e67
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-13 13:54:39 +0200

    Delete AllMakesQueryModel.cs

--- Project/All4Auto-main/All4Auto.Core/ViewModels/AllMakesQueryModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using All4Auto.Core.Models.Vehicles;
-
-namespace All4Auto.Core.ViewModels
-{
-    public class AllMakesQueryModel
-    {
-        public IEnumerable<MakeServiceModel> Makes { get; set; } = Enumerable.Empty<MakeServiceModel>();
-    }
-}
