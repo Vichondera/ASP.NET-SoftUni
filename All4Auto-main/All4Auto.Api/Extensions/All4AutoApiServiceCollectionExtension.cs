bc6b5416db7e6dea1d064f8335f86bacd364b181
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-12-13 13:43:32 +0200

    Add files via upload

---
+++ All4Auto-main/All4Auto.Api/Extensions/All4AutoApiServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using All4Auto.Core.Contracts;
+    using All4Auto.Core.Services;
+    using All4Auto.DataProcessor;
+    using All4Auto.DataProcessor.Common;
+    using Microsoft.EntityFrameworkCore;
+    public static class All4AutoApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IVehicleService, VehicleService>();
+           // services.AddScoped<IGuard, Guard>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAll4AutoDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<All4AutoDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
